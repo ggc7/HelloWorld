7860e8f5085930d40709c8d1c2fcad974ae99468
ggc7 <lxd.1990@gmail.com>
2014-01-06 15:59:58 +0800

    first commit. Create HEAD.

---
+++ src/HelloWorld/Helloworld.java
@@ -0,0 +1,17 @@
+package HelloWorld;
+
+public class Helloworld {
+	private int variable1 = 0;
+	private int variable2 = 0;
+	private int variable3 = 0;
+	
+	int main(){
+		System.out.println("hello world!");
+		variable1 = 1;
+		variable2 = 2;
+		variable3 = variable1 + variable2;
+		System.out.println(variable3);
+		return 0;
+		
+	}
+}
